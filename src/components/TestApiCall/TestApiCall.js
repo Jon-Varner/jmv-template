de341225b99f2f1cdf1a51330211ab910e6fbf76
Jon Varner <jon.varner@gmail.com>
2019-03-14 20:33:18 -0700

    Enabled the Test API call

--- src/components/TestApiCall/TestApiCall.js
+++ src/components/TestApiCall/TestApiCall.js
@@ -5,16 +5,23 @@ class TestApiCall extends Component {
   componentDidMount() {
     /* 
     NOTE: API calls will not work from localhost
-
-    this.props.userFetched(); 
     */
+    this.props.userFetched();
   }
 
   render() {
+    const user = this.props.user;
+    let id = 0;
+    let name = 'Unavailable';
+
+    if (user) {
+      id = user.id;
+      name = user.name;
+    }
+
     return (
       <p>
-        API returned User ID: {this.props.user.id} and Name:{' '}
-        {this.props.user.name}.
+        API returned User ID: {id} and Name: {name}.
       </p>
     );
   }
